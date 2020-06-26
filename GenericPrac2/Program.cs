c65787b7eb12f43cd838641e8f287091713dff42
JUDY <s2018s05@e-mirim.hs.kr>
2020-06-26 09:50:47 +0900

    초기 커밋.

---
+++ GenericPrac2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPrac2
+{
+    class Wanted<T>
+    {
+        public T Value;
+        public Wanted(T value)
+        {
+            this.Value = value;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wanted<string> wantedString = new Wanted<string>("Greeting");
+            Wanted<int> wantedInt = new Wanted<int>(100);
+            Wanted<double> wantedDouble = new Wanted<double>(3.141596);
+            Console.WriteLine("문자열 : " + wantedString.Value);
+            Console.WriteLine("문자열 : " + wantedInt.Value);
+            Console.WriteLine("문자열 : " + wantedDouble.Value);
+        }
+    }
+}
